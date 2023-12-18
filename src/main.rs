a5b0a927142f4031e6434a9a8828321a2f13f663
Peter Storm <peter.storm@peterstorm.io>
2023-12-18 11:14:16 +0100

    guessing_game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+use guessing_game::guessing_game::guessing_game_function;
+
+mod guessing_game;
+
+#[derive(Parser, Debug)]
+#[clap(author = "Peter Storm", version, about)]
+
+struct Args {
+    #[arg(short, long)]
+    program: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    match args.program {
+        Some(program) => {
+            match program.as_str() {
+                "guessing_game" => guessing_game_function(),
+                _ => println!("Unknown program: {}", program)
+            }
+        }
+        None => println!("Hello world! No program specified.")
+    }
 }
